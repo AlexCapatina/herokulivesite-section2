ce2a123213f871861ffe3358e628820a63044a4d
AlexCapatina <105219269+AlexCapatina@users.noreply.github.com>
2022-06-05 14:40:19 -0400

    added navbar and icons from FontAwesome

--- Server/Config/app.ts
+++ Server/Config/app.ts
@@ -1,15 +1,15 @@
 import createError from 'http-errors';
-import express, { NextFunction } from'express';
+import express, { NextFunction } from 'express';
 import path from'path';
 import cookieParser from 'cookie-parser';
-import logger from'morgan';
+import logger from 'morgan';
 
 import indexRouter from '../Routes/index';
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../Views'));
+app.set('Views', path.join(__dirname, '../Views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -22,7 +22,8 @@ app.use(express.static(path.join(__dirname, '../../node_modules')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next)
+{
   next(createError(404));
 });
 
